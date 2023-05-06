254d0e5beb16610d10fbaf944ece6f206367c0f9
nit0ss <nitoss@mail.com>
2023-05-06 20:09:08 +0200

    Shelf Class Constructor
    Here we add the desired parameters and call other methods to generate the entire structure

--- ShelfGenerator/src/models/Shelf.java
+++ ShelfGenerator/src/models/Shelf.java
@@ -8,31 +8,65 @@ import java.util.Scanner;
 @SuppressWarnings("unused")
 public class Shelf {
 
-	private double lenght;
-
-	private double thicknessShelves;
+	private double totalLength;
 	private int numShelves;
-	private final double minseparation;
-	private final double minthickness = 1;
-	private final double maxthickness;
-	private final double xAxis = 20.0;
-	public double width;
+	private int minseparation;
+	private final double thickness = 5.0; // each shelf has a prefab thickness
+	private final double xAxis = 20.0; // each shelf is 20cm wide
+	private double shelveLength;
+	// public double width;
 	Scanner sc = new Scanner(System.in);
-	
-	public Shelf(double lenght, double width, double thicknessShelves, int numShelves) {
-		super();
-		this.lenght = lenght;
-		this.thicknessShelves = thicknessShelves;
+
+	String topShelveOBJ;
+
+	public Shelf(double totalLength, int numShelves, double shelveLength) {
+
+		this.totalLength = totalLength;
 		this.numShelves = numShelves;
-		this.minseparation = lenght/numShelves;
-		
-		this.maxthickness = (thicknessShelves)*numShelves;
-		//minimo de cada balda de grososr tiene que ser 1 cm, entonces para 170cm,7baldas->no podria haber 24 baldas por el grosor
-			
+		this.shelveLength = shelveLength;
+
+		// this.maxthickness = (thicknessShelves)*numShelves;
+		// minimo de cada balda de grososr tiene que ser 1 cm, entonces para
+		// 170cm,7baldas->no podria haber 24 baldas por el grosor
+
+	}
+
+	public double getLenght() {
+		return totalLength;
+	}
+
+	public int getNumShelves() {
+		return numShelves;
+	}
+
+	public int getMinseparation() {
+		return minseparation;
+	}
+
+	public double getTotalLength() {
+		return totalLength;
+	}
+
+	public double getShelveLength() {
+		return shelveLength;
+	}
+
+	public double getThickness() {
+		return thickness;
+	}
+
+	public double getxAxis() {
+		return xAxis;
 	}
 	
-	
+	public void setMinSeparation(double totalLength, int numShelves, double thickness){
+		this.minseparation = (int) (totalLength / (numShelves - (2 * thickness)));
+	}
 
-
+	@Override
+	public String toString() {
+		return "Shelf [lenght=" + totalLength + ", numShelves=" + numShelves + ", minseparation=" + minseparation
+				+ ", thickness=" + thickness + ", xAxis=" + xAxis + "]";
+	}
 
 }
