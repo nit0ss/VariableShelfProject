6c64a86aeaf99e3f24f4c864d43576fff9d71f3a
nit0ss <nitoss@mail.com>
2023-05-06 20:09:30 +0200

    Shelve v1
    This one will be cloned in order to create the number of desired shelves

---
+++ ShelfGenerator/src/models/Shelve.java
@@ -0,0 +1,116 @@
+package models;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Shelve {
+
+	private final double zAxis = 20.0;
+
+	private final double width = 5.0;
+	private double totalLength;
+	private final double length;
+
+	public Shelve(double totalLength, double shelveLength) {
+		this.totalLength = totalLength;
+		this.length = shelveLength;
+	}
+
+	public String getObjVector() {
+
+		String result = "";
+		
+		//8 vertex 8 duples
+		double x1 = 0.00 ;
+		double y1 = totalLength;
+		double z1 = zAxis;
+
+		double x2 = 0.00;
+		double y2 = totalLength;
+		double z2 = 0.00;
+
+		double x3 = length;
+		double y3 = totalLength;
+		double z3 = 0.00;
+
+		double x4 = length;
+		double y4 = totalLength;
+		double z4 = zAxis;
+
+		double x5 = 0.00;
+		double y5 = totalLength-width;
+		double z5 = zAxis;
+
+		double x6 = 0.00;
+		double y6 = totalLength-width;
+		double z6 = 0.00;
+
+		double x7 = length;
+		double y7 = totalLength-width;
+		double z7 = 0.00;
+
+		double x8 = length;
+		double y8 = totalLength-width;
+		double z8 = zAxis;
+		
+		
+		result += "v " + x1 + " " + y1 + " " + z1 + "\n";
+        result += "v " + x2 + " " + y2 + " " + z2 + "\n";
+        result += "v " + x3 + " " + y3 + " " + z3 + "\n";
+        result += "v " + x4 + " " + y4 + " " + z4 + "\n";
+        result += "v " + x5 + " " + y5 + " " + z5 + "\n";
+        result += "v " + x6 + " " + y6 + " " + z6 + "\n";
+        result += "v " + x7 + " " + y7 + " " + z7 + "\n";
+        result += "v " + x8 + " " + y8 + " " + z8 + "\n";
+        
+		// Define the face indices for the cube
+		int face1v1 = 1, face1v2 = 2, face1v3 = 3, face1v4 = 4;
+		int face2v1 = 5, face2v2 = 6, face2v3 = 7, face2v4 = 8;
+		int face3v1 = 1, face3v2 = 2, face3v3 = 6, face3v4 = 5;
+		int face4v1 = 2, face4v2 = 3, face4v3 = 7, face4v4 = 6;
+		int face5v1 = 3, face5v2 = 4, face5v3 = 8, face5v4 = 7;
+		int face6v1 = 1, face6v2 = 4, face6v3 = 8, face6v4 = 5;
+
+		// Append the face indices to the result string
+		result += "f " + face1v1 + " " + face1v2 + " " + face1v3 + " " + face1v4 + "\n";
+		result += "f " + face2v1 + " " + face2v2 + " " + face2v3 + " " + face2v4 + "\n";
+		result += "f " + face3v1 + " " + face3v2 + " " + face3v3 + " " + face3v4 + "\n";
+		result += "f " + face4v1 + " " + face4v2 + " " + face4v3 + " " + face4v4 + "\n";
+		result += "f " + face5v1 + " " + face5v2 + " " + face5v3 + " " + face5v4 + "\n";
+		result += "f " + face6v1 + " " + face6v2 + " " + face6v3 + " " + face6v4 + "\n";
+
+
+		System.out.println("Created Top Shelve with the coordinates :\n" + result);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// Generate the vertex string
+		Shelve obj = new Shelve(160.0, 60.0);
+		String vertexString = obj.getObjVector();
+
+		// Export the vertex string to a TXT file
+		String fileName = "result.txt";
+		try {
+			FileWriter writer = new FileWriter(fileName);
+			writer.write(vertexString);
+			writer.close();
+			System.out.println("Vertex string exported to " + fileName + " successfully.");
+		} catch (IOException e) {
+			System.out.println("An error occurred while exporting the vertex string to " + fileName + ".");
+			e.printStackTrace();
+		}
+
+		try {
+            BufferedWriter txtWriter = new BufferedWriter(new FileWriter("result.obj"));
+            txtWriter.write(vertexString);
+            txtWriter.close();
+            System.out.println("Text file saved as result.obj");
+        } catch (IOException e) {
+            System.out.println("Error saving text file: " + e.getMessage());
+        }
+    }
+	
+
+}
